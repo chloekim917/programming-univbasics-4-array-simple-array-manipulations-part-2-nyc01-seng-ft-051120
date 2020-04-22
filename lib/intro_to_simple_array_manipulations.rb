460a7926a092b4f6cad405dde6e0d13e75e4aec6
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 20:37:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,17 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert.4.element
+  array.insert(4,element)
 end
 
+def flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
